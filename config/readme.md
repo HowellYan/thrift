dd02d0645b600cc1bce015e9f03ea1b663244b97
Howell <Howell@Howells-MBP.local>
2017-07-12 12:57:42 +0800

    start

--- config/readme.md
+++ config/readme.md
@@ -1,3 +1,5 @@
 ```
 docker run -v "/Users/Howell/Work/other/docker/thrift:/data" thrift thrift -o /data --gen java /data/add.thrift
 ```
+
+/usr/local/Cellar/thrift/0.10.0/bin
--- thrift-api/src/main/java/cn/com/howell/thrift/AdditionService.thrift
+++ thrift-api/src/main/java/cn/com/howell/thrift/AdditionService.thrift
@@ -3,4 +3,5 @@ typedef i32 int  //typedefs to get convenient names for your types
 service AdditionService {  // defines the service to add two numbers  
         int add(1:int n1, 2:int n2), //defines a method  
         string getString(1:string s1),
+        int tre(1:int n1, 2:int n2, 3:int n3)
 }
