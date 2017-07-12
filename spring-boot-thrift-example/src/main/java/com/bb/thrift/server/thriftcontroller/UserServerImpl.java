c38a0b6c1005390724d5c4327cd3aba960c2675c
Howell <Howell@Howells-MBP.local>
2017-07-12 19:37:41 +0800

    start

---
+++ spring-boot-thrift-example/src/main/java/com/bb/thrift/server/thriftcontroller/UserServerImpl.java
@@ -0,0 +1,29 @@
+package com.bb.thrift.server.thriftcontroller;
+
+
+import cn.com.howell.api.UserService;
+import cn.com.howell.api.req.UserReq;
+import cn.com.howell.api.resp.UserResp;
+import cn.com.howell.api.resp.base.BaseResp;
+import org.apache.thrift.TException;
+import org.spring.boot.thrift.server.annotation.ThriftServer;
+
+/**
+ * Created by bob on 17/1/11.
+ */
+@ThriftServer(value = "/userService")
+public class UserServerImpl implements UserService.Iface {
+
+    @Override
+    public UserResp userLogin(UserReq userReq) throws TException {
+        UserResp userResp = new UserResp();
+        BaseResp baseResp = new BaseResp();
+        baseResp.setCode("000000").setMsg("成功").setIp("123456345345");
+        if(userReq.getUserId().equals("123456")){
+            userResp.setBaseResp(baseResp);
+            userResp.setUserId("123456777");
+            userResp.setUserName("1234567777");
+        }
+        return userResp;
+    }
+}
