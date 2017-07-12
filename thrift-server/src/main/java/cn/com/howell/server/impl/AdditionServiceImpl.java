803ca3d8a26e448f47eed11923c3207eb57c2847
Howell <Howell@Howells-MBP.local>
2017-07-12 13:40:12 +0800

    start

--- thrift-server/src/main/java/cn/com/howell/server/impl/AdditionServiceImpl.java
+++ thrift-server/src/main/java/cn/com/howell/server/impl/AdditionServiceImpl.java
@@ -8,12 +8,18 @@ import org.apache.thrift.TException;
  * E-Mail:th15817161961@gmail.com
  */
 public class AdditionServiceImpl implements AdditionService.Iface {
+    @Override
     public int add(int n1, int n2) throws TException {
-
         return n1+n2;
     }
 
+    @Override
     public String getString(String s1) throws TException {
         return s1+" , 你好！";
+    }
+
+    @Override
+    public int tre(int n1, int n2, int n3) throws TException {
+        return n1+n2+n3;
     }
 }
