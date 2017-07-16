6437bd585aa23e85537bd54aad8088731884409f
howell <th15817161961@gmail.com>
2017-07-16 12:12:37 +0800

    v1.0.2

---
+++ spring-boot-thrift-example/src/main/java/com/howell/thrift/server/ThriftApp.java
@@ -0,0 +1,30 @@
+package com.howell.thrift.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Howell on 17/1/11.
+ */
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+public class ThriftApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThriftApp.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "Starting......";
+    }
+
+    @RequestMapping("/health")
+    public String health() {
+        return "{\"description\":\"Spring Cloud Consul Discovery Client\",\"status\":\"UP\"}";
+    }
+}
---
+++ spring-boot-thrift-example/src/main/java/com/howell/thrift/server/thriftcontroller/UserServerImpl.java
@@ -0,0 +1,34 @@
+package com.howell.thrift.server.thriftcontroller;
+
+
+import cn.com.howell.api.UserService;
+import cn.com.howell.api.req.UserReq;
+import cn.com.howell.api.resp.UserResp;
+import cn.com.howell.api.resp.base.BaseResp;
+import org.apache.thrift.TException;
+import org.spring.boot.thrift.server.annotation.ThriftServer;
+
+/**
+ * Created by Howell on 17/1/11.
+ */
+@ThriftServer(value = "/userService")
+public class UserServerImpl implements UserService.Iface {
+
+    @Override
+    public UserResp userLogin(UserReq userReq) throws TException {
+        UserResp userResp = new UserResp();
+        BaseResp baseResp = new BaseResp();
+        baseResp.setCode("000000").setMsg("成功").setIp("123456345345");
+        if(userReq.getUserId().equals("123456")){
+            userResp.setBaseResp(baseResp);
+            userResp.setUserId("123456777");
+            userResp.setUserName("1234567777");
+        }
+        return userResp;
+    }
+
+    @Override
+    public UserResp userLogout(UserReq userReq) throws TException {
+        return null;
+    }
+}
